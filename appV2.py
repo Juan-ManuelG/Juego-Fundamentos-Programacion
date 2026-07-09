1975be2a72b429ff4bee0966c0382cc8d9acc9a7
Juan <jguzmanpenagos@gmail.com>
2026-07-09 10:18:16 -0500

    actualizacion

--- appV2.py
+++ appV2.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+import random
+
 def mostrarCartas(nombre):
     print(f"¡{nombre} mantén tus ojos bien abiertos mientras las cartas se mueven!")
 
     carta1=["Q", "♥"]#carta ganadora
     carta2=["8", "♣"]
     carta3=["J", "♦"]
-
+    posiciones=["Q", "8", "J"]
+    posicionGanadora=posiciones[0]
+    posicionGanadora = random.choice(["Q", "8", "J"])
     print("┌───┐ ┌───┐ ┌───┐")
     print(f"|{carta1[0]}  | |{carta2[0]}  | |{carta3[0]}  |")
     print(f"| {carta1[1]} | | {carta2[1]} | | {carta3[1]} |")
@@ -18,16 +23,22 @@ def mostrarCartas(nombre):
     print("| ? | | ? | | ? |")
     print("|   | |   | |   |")
     print("└───┘ └───┘ └───┘")
-    print("Intercambio izquierda (I) con la del medio (M)...")
-    print("Intercambio derecha (D) con la del medio (M)...")
-    print("Intercambio la del medio (M) con izquierda (I)...")
-    print("Intercambio derecha (D) con izquierda (I)...")
-    print("Intercambio izquierda (I) con derecha (D)...")
-    cartaCorrecta(carta1, carta2, carta3)
+    intercambios=[
+        ("I", "M", "izquierda (I) con la del medio (M)"),
+        ("D", "M", "derecha (D) con la del medio (M)"),
+        ("M", "I", "la del medio (M) con izquierda (I)"),
+        ("D", "I", "derecha (D) con izquierda (I)"),
+        ("I", "D", "izquierda (I) con derecha (D)"),
+        ]
+    
+    movimientosIntercambio=random.sample(intercambios, 5) #movimientos
+    for mueve in movimientosIntercambio:
+        print(f"Intercambio {mueve}")
+    cartaCorrecta(carta1, carta2, carta3, nombre, posicionGanadora)
 
 
 def jugar():
-    
+
     print("Adivina dónde está la reina de corazones")
     nombre=input("Ingrese su nombre de usuario: ").capitalize()
     accion=input(f"{nombre} seleccione jugar [J], tabla de posiciones [T], salir [S]: ").upper()
@@ -40,14 +51,14 @@ def jugar():
             mostrarCartas(nombre)
 
         elif accion=="T":
-            tablaPosiciones(nombre, puntosGanador)
+            tablaPosiciones()
 
         accion=input(f"{nombre} seleccione jugar [J], tabla de posiciones [T], salir [S]: ").upper()
 
     if accion=="S":
         print("¡Adiós!")
     
-def cartaCorrecta(carta1, carta2, carta3):
+def cartaCorrecta(carta1, carta2, carta3, nombre, posicionGanadora):
     opcionCarta=input("¿En cuál de las cartas está la reina de corazones? [I], [M], [D]: ").upper()
     
 
@@ -55,9 +66,13 @@ def cartaCorrecta(carta1, carta2, carta3):
       opcionCarta=input("¿En cuál de las cartas está la reina de corazones? [I], [M], [D]: ").upper()
     
     
-    if opcionCarta == "D":
+    if opcionCarta == posicionGanadora:
         puntosGanador=5
-        print(f"La opcion {opcionCarta} es CORRECTA ¡Haz recibido +{puntosGanador} puntos!")
+        print(f"{nombre}, la opcion {opcionCarta} es CORRECTA ¡Haz recibido +{puntosGanador} puntos!")
+        fecha=datetime.now().strftime("%d/%m/%Y")
+        hora=datetime.now().strftime("%H/%M")
+        with open("puntajes.txt", "a") as puntaje:
+            print(f"{nombre}, {puntosGanador} puntos. Jugada {fecha} {hora}", file=puntaje)
     elif opcionCarta =='I' or opcionCarta=='M':
         print("Lo siento perdedor(a) :-(")
         print("¡Gracias por jugar!")
@@ -65,14 +80,11 @@ def cartaCorrecta(carta1, carta2, carta3):
     ordenFinal(carta1, carta2, carta3)
 
     
-def tablaPosiciones(nombre, puntosGanador):
+def tablaPosiciones():
+    with open("puntajes.txt", "r") as puntajes:
+        for points in puntajes:
+            print(points.strip())
     
-    print("Top 5 de lo(a)s mejores:")
-    print(f"1. {nombre}, {puntosGanador} puntos, Mejor jugada 2022-03-11 a las 10:58pm")
-    print("2. Rodolfo, 8 puntos. Mejor jugada el 2026-03-12 a las 08:00am")
-    print("3. Lucia, 7 puntos. Mejor jugada el 2026-02-10 a las 9:10pm")
-    print("4. Rocio, 3 puntos. Mejor jugada el 2026-02-08 a las 10:13pm")
-    print("5. Jhon, 2 puntos. Mejor jugada el 2026-03-07 a las 7:10pm")
     
     
 
